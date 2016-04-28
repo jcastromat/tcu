39f5c5f9b1c894d377df853a18f8d3fa8e779322
Javier Castro <Javier@Javiers-Air.attlocal.net>
2016-04-28 00:04:33 -0700

    form-based workflow feature for items completed

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,3 +1,5 @@
+include ItemsHelper
+
 class ItemsController < ApplicationController
 
   def index
@@ -6,6 +8,35 @@ class ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to item_path(@item)
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+    redirect_to items_path
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+
+    flash.notice = "Item '#{@item.name}' Updated!"
+
+    redirect_to item_path(@item)
   end
 
 end
